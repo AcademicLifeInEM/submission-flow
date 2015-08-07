7f760817b85bc2a19a0f91ba2cf754c53a656036
dsifford <dereksifford@gmail.com>
2015-08-07 17:51:58 -0400

    Update JS hides

--- inc/js/dashboard-hide.js
+++ inc/js/dashboard-hide.js
@@ -3,7 +3,6 @@ jQuery(document).ready(function($) {
     /**
      * HIDE EVERYTHING BY DEFAULT (explicit shows following)
      */
-    // $('#wp-admin-bar-root-default > li').hide(); // Admin Bar Links
     $('#wp-admin-bar-new-media').hide();
     $('#adminmenu > li').hide(); // Admin Menu (sidebar) Links
     $('#normal-sortables').children().hide(); // First set of sortables
@@ -13,7 +12,17 @@ jQuery(document).ready(function($) {
     $('#screen-options-link-wrap').hide();
     $('#postimagediv').hide();
     $('#latest-comments').hide();
-    // $('#misc-publishing-actions').children().hide();
+    $('#add_poll').hide();
+    $('#wp-admin-bar-ab-ls-add-new').hide();
+    $('#fusion-pb-switch-button').hide();
+    $('#pyre_page_options').hide();
+    $('#pageparentdiv').hide();
+    $('#featured-image-2_page').hide();
+    $('#featured-image-3_page').hide();
+    $('#featured-image-4_page').hide();
+    $('#featured-image-5_page').hide();
+    $('.misc-pub-section misc-yoast').hide();
+    $('.misc-pub-section misc-pub-visibility').hide();
 
     /**
      * EXPLICIT SHOWS
@@ -21,6 +30,7 @@ jQuery(document).ready(function($) {
     // ADMIN BAR LINKS
     $('.wp-admin-bar-wp-logo').show();
     $('.wp-admin-bar-site-name').show();
+
 
     // SIDEBAR LINKS
     $('#menu-dashboard').show();
