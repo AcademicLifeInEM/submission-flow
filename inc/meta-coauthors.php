a4d1901c8b8dc1aec2ac9bde05c8327f33794f7b
dsifford <dereksifford@gmail.com>
2015-08-08 19:51:15 -0400

    Fix sizing issue - Coauthor area

--- inc/meta-coauthors.php
+++ inc/meta-coauthors.php
@@ -9,7 +9,7 @@
                 <label for="coauthor_1_email">Email</label>
                 <input type="text" name="coauthor_1_email" id="coauthor_1_email" value="<?php echo($coauthor_1_email); ?>">
                 <label for="coauthor_1_twitter">Twitter</label>
-                <input type="text" name="coauthor_1_twitter" id="coauthor_1_twitter" value="<?php echo($coauthor_1_twitter); ?>">
+                <input type="text" name="coauthor_1_twitter" id="coauthor_1_twitter" size="15" value="<?php echo($coauthor_1_twitter); ?>">
                 <label for="coauthor_1_background">Background Info</label>
                 <textarea style="vertical-align: middle; padding: 3px 5px; resize: vertical; resize: vertical;" name="coauthor_1_background" rows="1" cols="30"><?php echo( parse_html_chars( $coauthor_1_background ) ); ?></textarea>
             </div>
@@ -21,7 +21,7 @@
                 <label for="coauthor_2_email">Email</label>
                 <input type="text" name="coauthor_2_email" id="coauthor_2_email" value="<?php echo($coauthor_2_email); ?>">
                 <label for="coauthor_2_twitter">Twitter</label>
-                <input type="text" name="coauthor_2_twitter" id="coauthor_2_twitter" value="<?php echo($coauthor_2_twitter); ?>">
+                <input type="text" name="coauthor_2_twitter" id="coauthor_2_twitter" size="15" value="<?php echo($coauthor_2_twitter); ?>">
                 <label for="coauthor_2_background">Background Info</label>
                 <textarea style="vertical-align: middle; padding: 3px 5px; resize: vertical;" name="coauthor_2_background" rows="1" cols="30"><?php echo( parse_html_chars( $coauthor_2_background ) ); ?></textarea>
             </div>
@@ -33,7 +33,7 @@
                 <label for="coauthor_3_email">Email</label>
                 <input type="text" name="coauthor_3_email" id="coauthor_3_email" value="<?php echo($coauthor_3_email); ?>">
                 <label for="coauthor_3_twitter">Twitter</label>
-                <input type="text" name="coauthor_3_twitter" id="coauthor_3_twitter" value="<?php echo($coauthor_3_twitter); ?>">
+                <input type="text" name="coauthor_3_twitter" id="coauthor_3_twitter" size="15" value="<?php echo($coauthor_3_twitter); ?>">
                 <label for="coauthor_3_background">Background Info</label>
                 <textarea style="vertical-align: middle; padding: 3px 5px; resize: vertical;" name="coauthor_3_background" rows="1" cols="30"><?php echo( parse_html_chars( $coauthor_3_background ) ); ?></textarea>
             </div>
@@ -45,7 +45,7 @@
                 <label for="coauthor_4_email">Email</label>
                 <input type="text" name="coauthor_4_email" id="coauthor_4_email" value="<?php echo($coauthor_4_email); ?>">
                 <label for="coauthor_4_twitter">Twitter</label>
-                <input type="text" name="coauthor_4_twitter" id="coauthor_4_twitter" value="<?php echo($coauthor_4_twitter); ?>">
+                <input type="text" name="coauthor_4_twitter" id="coauthor_4_twitter" size="15" value="<?php echo($coauthor_4_twitter); ?>">
                 <label for="coauthor_4_background">Background Info</label>
                 <textarea style="vertical-align: middle; padding: 3px 5px; resize: vertical;" name="coauthor_4_background" rows="1" cols="30"><?php echo( parse_html_chars( $coauthor_4_background ) ); ?></textarea>
             </div>
